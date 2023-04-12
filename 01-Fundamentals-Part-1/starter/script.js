25ae3139df0c9d905c7d8ed0563ee225a580f3c3
Yanir Haim <yanir.haimescobar@post.runi.ac.il>
2023-04-12 19:45:55 +0300

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,10 @@
+// Values and Variables
 let country = 'Ecuador';
 let continent = 'America';
 let population = '18.00 million';
 
 console.log("I live in " + country + ", located in " + continent + ", which has a population of around " + population);
+
+// Data Types
+let isIsland = false;
+console.log(isIsland ? country + " is an Island" : country + " is not an Island");
