26cab39acae023a0f919145e2cf85c7008b6b23d
Yanir Haim <yanir.haimescobar@post.runi.ac.il>
2023-04-12 20:16:29 +0300

    Part1 - Finished

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,4 +7,30 @@ console.log("I live in " + country + ", located in " + continent + ", which has 
 
 // Data Types
 let isIsland = false;
+const language = 'Spanish';
 console.log(isIsland ? country + " is an Island" : country + " is not an Island");
+
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+
+let markMass, markHeight, johnMass, johnHeight, markBMI, johnBMI;
+markMass = 78;
+markHeight = 1.69;
+johnMass = 92;
+johnHeight = 1.95;
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
